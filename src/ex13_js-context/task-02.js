fba0e8294a47d749c609721935ae19ad9825f491
Popov Matvey <85041400+PopovMatvey@users.noreply.github.com>
2022-05-14 20:55:07 +0300

    Тринадцатая лабораторная работа выполнена
    Были выполнены все два задания: 
    1. Реализован калькулятор с использованием контекста и игра "Виселица";
    2. Были выполнены все тесты и линтеры.

---
+++ src/ex13_js-context/task-02.js
@@ -0,0 +1,84 @@
+function createGuessedString(guessWord) {
+  return guessWord.replace(/./g, '_');
+}
+
+function checkString(guessWord, inputChart) {
+  return guessWord.indexOf(inputChart) > -1;
+}
+
+function changeString(guessWord, guessedString, inputChart) {
+  const splitGuessedString = guessedString.split('');
+  const splitGuessWord = guessWord.split('');
+
+  for (let i = 0; i < splitGuessWord.length; i++) {
+    if (splitGuessWord[i] === inputChart) {
+      splitGuessedString[i] = inputChart;
+    }
+  }
+
+  return splitGuessedString.join('');
+}
+
+function Hangman(guessWord) {
+  this.guessWord = guessWord;
+  this.guessedString = createGuessedString(guessWord);
+}
+
+const hangman = {
+
+  guessWord: '',
+  guessedString: '',
+  errorsLeft: 6,
+  wrongSymbols: [],
+
+  guess(inputChart) {
+    if (checkString(this.guessWord, inputChart)) {
+      this.guessedString = changeString(// eslint-disable-line no-return-assign
+        this.guessWord, this.guessedString, inputChart,
+      );
+      console.log(this.guessedString);
+    } else {
+      this.errorsLeft--;
+
+      if (this.wrongSymbols.indexOf(inputChart) === -1) {
+        this.wrongSymbols.push(inputChart);
+      }
+
+      console.log(`wrong letter, errors left ${this.errorsLeft} | ${this.wrongSymbols}`);
+    }
+
+    return this;
+  },
+
+  getGuessedString() {
+    return this.guessedString;
+  },
+
+  getErrorsLeft() {
+    return this.errorsLeft;
+  },
+
+  getWrongSymbols() {
+    return this.wrongSymbols;
+  },
+
+  getStatus() {
+    return `${this.guessedString} | errors left ${this.errorsLeft}`;
+  },
+
+  startAgain(guessWord) {
+    this.guessWord = guessWord;
+    this.guessedString = createGuessedString(guessWord);
+    this.errorsLeft = 6;
+  },
+};
+
+Hangman.prototype = hangman;
+
+const game = new Hangman('aboba');
+
+console.log(game.guess('b').guess('a'));
+
+console.log(game.getStatus());
+
+module.exports = Hangman.prototype;
